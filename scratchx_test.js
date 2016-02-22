22a6ce22022720ad015a4f0a5d7cf65f1bee0d7c
takuto narisawa <takunari.lego@gmail.com>
2016-02-22 13:04:58 +0900

    ScratchX_3
    ブロックの追加

--- scratchx_test.js
+++ scratchx_test.js
@@ -1,3 +1,6 @@
+var color;
+
+
 (function(ext) {
     // Cleanup function when the extension is unloaded
     ext._shutdown = function() {};
@@ -12,10 +15,18 @@
         alert("Hello_scratchx")
         // Code that gets executed when the block is run
     };
-
+    ext.set_color = function(Red,Green,Blue) {
+        color=Red*0x10000+Green*0x100+Blue
+    };
+    ext.send_color = function() {
+            return color;
+        };
     // Block and block menu descriptions
     var descriptor = {
-        blocks: [[' ', 'Hello_scratchx', 'Hello_scratchx'],
+        blocks: [
+          [' ', 'Hello_scratchx', 'Hello_scratchx'],
+          [' ', 'set color R:%n, G:%n, B:%n', 'set_color',0,0,0],
+          ['r', 'color','send_color'],
         ]
     };
 
