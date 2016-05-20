632623c7342a1f382b4201c50966cd4ab87dbce0
takuto narisawa <takunari.lego@gmail.com>
2016-05-21 05:24:20 +0900

    accel7
    大きさだけにまとめた

--- droid.js
+++ droid.js
@@ -3,9 +3,7 @@ var red=0;
 var blue=0;
 var green=0;
 var yellow=0;
-var x_accel=0;
-var y_accel=0;
-var z_accel=0;
+var accel=0;
 var read_accel=0;
 (function(ext) {
     var device=null;
@@ -30,20 +28,9 @@ var read_accel=0;
         device.set_receive_handler(function(data) {
           var mes= new Uint8Array(data);
           console.log(mes);
-          switch (read_accel) {
-            case 3:
-              x_accel=mes[0]/10;
-              read_accel--;
-              break;
-            case 2:
-              y_accel=mes[0]/10;
-              read_accel--;
-              break;
-            case 1:
-              z_accel=mes[0]/10;
-              read_accel--;
-              break;
-            case 0:
+          if(read_accel=1){
+              accel=mes[0]/25;
+          }else{
               if(mes[0]===16){
                 read_accel=3;
               }else if(mes[0]===48){//receive message
@@ -59,31 +46,15 @@ var read_accel=0;
                   yellow=(mes[0]&8)/8;
                 }
               }
-          default:
-              break;
           }
 
         });
       }
 
-      ext.when_accel = function(axis,sign,level) {
-          switch (axis) {
-            case 'x':
-              var accel=x_accel;
-            case 'y':
-              var accel=y_accel;
-            case 'z':
-              var accel=z_accel;
-            case 'abs':
-              var accel=abs_accel;
-          }
-          if (sign==">"){return accel>level;}else{return accel<level;}
+      ext.when_accel = function(sign,level) {
+        if (sign==">"){return accel>level;}else{return accel<level;}
       };
-
-
-
-
-    ext.when_push = function(color) {
+      ext.when_push = function(color) {
         switch (color) {
           case 'blue':
             return(blue===1);
@@ -110,36 +81,23 @@ var read_accel=0;
     ext.get_yellow = function() {
             return yellow;
     };
-    ext.get_x_accel = function() {
-            return x_accel;
-    };
-    ext.get_y_accel = function() {
-            return y_accel;
-    };
-    ext.get_z_accel = function() {
-            return z_accel;
-    };
-    ext.get_abs_accel = function() {
-            return sqrt(x_accel^2+y_accel^2+z_accel^2);
+    ext.get_accel = function() {
+            return accel
     };
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
           ['h', 'when %m.color button pushed', 'when_push','blue'],
-          ['h', 'when %m.axis _accel %m.lessmore %d','when_accel', 'abs','>',10],
+          ['h', 'when accel %m.lessmore %d','when_accel','>',10],
           ['r', 'message','get_message'],
           ['r', 'red_btn','get_red'],
           ['r', 'blue_btn','get_blue'],
           ['r', 'green_btn','get_green'],
           ['r', 'yellow_btn','get_yellow'],
-          ['r', 'x_accel','get_x_accel'],
-          ['r', 'y_accel','get_y_accel'],
-          ['r', 'z_accel','get_z_accel'],
-          ['r', 'abs_accel','get_abs_accel'],
+          ['r', 'accel','get_accel'],
         ],
         menus: {
           color: ['blue ', 'red', 'green', 'yellow'],
-          axis:['x', 'y', 'z', 'abs'],
           lessMore: ['>', '<']
         }
     };
