aa53294654f2bcf98ff06698b38f28f9ce57bd95
takuto narisawa <takunari.lego@gmail.com>
2016-11-22 11:20:13 +0900

    hid14

--- csv.js
+++ csv.js
@@ -20,7 +20,7 @@ new (function() {
         // otherwise start polling
 
         poller = setInterval(function() {
-            input = device.read(callback,48);
+            input =  new Uint8Array(device.read(callback,48));
             console.log(input);
         }, 20);
 
