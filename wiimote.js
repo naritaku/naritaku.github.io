6434e5ab11dd92a90ac36939fe2eabdab6b0c1b7
takuto narisawa <takunari.lego@gmail.com>
2016-12-05 06:55:29 +0900

    wii3
    値の読み取り

--- wiimote.js
+++ wiimote.js
@@ -1,19 +1,28 @@
-
+//  button['a','b','up','down','left','right','1','2','+','-','home'],
+var btn_state=[0,0,0,0,0,0,0,0,0,0,0];
+var dateMode=15;//states check
 new (function() {
     var device = null;
     var input = null;
     var poller = null;
     var ext = this;
-    function write_callback(data) {
-      console.log(data);
-      console.log(data[0]);
-      return new Float64Array(data);
-    };
+
     function read_callback(data) {
-        console.log(data);
-        console.log(data[1]);
-        console.log(data[2]);
-        return new Float64Array(data);
+        return data;
+    };
+
+    function btn_update(data) {
+        btn_state[0]=data[2]&0X08/0X08;
+        btn_state[1]=data[2]&0X04/0X04;
+        btn_state[2]=data[1]&0X08/0X08;
+        btn_state[3]=data[1]&0X04/0X04;
+        btn_state[4]=data[1]&0X01/0X01;
+        btn_state[5]=data[1]&0X02/0X02;
+        btn_state[6]=data[2]&0X02/0X02;
+        btn_state[7]=data[2]&0X01/0X01;
+        btn_state[8]=data[1]&0X10/0X10;
+        btn_state[9]=data[2]&0X10/0X10;
+        btn_state[10]=data[2]&0X80/0X80;
     };
 
     function deviceOpened(dev) {
@@ -24,8 +33,8 @@ new (function() {
 
         poller = setInterval(function() {
             device.write(0x1500);
-            input =  device.read(read_callback,48);
-            console.log(input);
+            var data =  device.read(read_callback,48);
+            btn_update(data);
         }, 20);
 
     };
@@ -65,7 +74,30 @@ new (function() {
     // Converts a byte into a value of the range -1 -> 1 with two decimal places of precision
 //    function convertByteStr(byte) { return (parseInt(byte, 16) - 128) / 128; }
     ext.send_button = function(buttton) {
-      return button;
+      switch (button) {
+        case 'a':
+          return(btn_state[0]);
+        case 'b':
+          return(btn_state[1]);
+        case 'up':
+          return(btn_state[2]);
+        case 'down':
+            return(btn_state[3]);
+        case 'left':
+            return(btn_state[4]);
+        case 'right':
+            return(btn_state[5]);
+        case '1':
+            return(btn_state[6]);
+        case '2':
+            return(btn_state[7]);
+        case '+':
+            return(btn_state[8]);
+        case '-':
+            return(btn_state[9]);
+        case 'home':
+            return(btn_state[10]);
+      }
     }
     var descriptor = {
         blocks: [
@@ -73,7 +105,7 @@ new (function() {
 
         ],
         menus: {
-          button:['a','b','1','2','+','-','home'],
+          button:['a','b','up','down','left','right','1','2','+','-','home'],
         }
     };
     ScratchExtensions.register('Wiimote', descriptor, ext, {type: 'hid', vendor:0x057e, product:0x0306});
