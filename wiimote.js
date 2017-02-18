a3d0a5cac75f1e66b3b9cc8d6051400f17021cc4
takuto narisawa <takunari.lego@gmail.com>
2017-02-18 15:03:37 +0900

    wiimotetest

--- wiimote.js
+++ wiimote.js
@@ -1,7 +1,7 @@
 var btn_state=[0,0,0,0,0,0,0,0,0,0,0];
 var LED=['□□□□','□□□■','□□■□','□□■■','□■□□','□■□■','□■■□','□■■■','■□□□','■□□■','■□■□','■□■■','■■□□','■■□■','■■■□','■■■■'];
 var LED_RUMBLE=[0x11,0x00];
-var SETUP=[0x12,0x04,0x30];
+var SETUP=[0x12,0x04,0x31];
 var GETSTATE=[0x15,0x00];
 (function(ext) {
     var device = null;
@@ -26,25 +26,19 @@ var GETSTATE=[0x15,0x00];
     };
 
     function deviceOpened(dev) {
-        // if device fails to open, forget about it
         if (dev == null) device = null;
-        //device.write(GETSTATE);
-        //device.write(SETUP);
         device.write(new Uint8Array(SETUP).buffer);
+				device.read(read_callback,48);
         device.write(new Uint8Array(GETSTATE).buffer);
         poller = setInterval(function() {
-            device.write(new Uint8Array(SETUP).buffer);
             device.read(read_callback,64);
             console.log()
         }, 62.5);
     };
     ext._deviceConnected = function(dev) {
         if(device) return;
-
         device = dev;
         device.open(deviceOpened);
-
-//        setInterval(function() { LED_RUMBLE }, 100);
     };
 
     ext._deviceRemoved = function(dev) {
@@ -105,25 +99,20 @@ var GETSTATE=[0x15,0x00];
     }
 
     ext.trunOnLED = function(led) {
-    /*  for (var i=0 ; i<=15 ; i++){}
-        if (led=== LED[i]){
-          led_state=i
-          console.log(led_state);
-          led_rumble[2]=led_rumble[2]%16+led_state*16;
-          device.write(led_rumble);
-          device.read(read_callback,64);
-        }*/
+				for (var led_num=0 ; led_num<16 ; led_num++){
+  				if(LED[led_num]===led){break;};
+				}
+				LED_RUMBLE[1]=led*16;
         device.write(new Uint8Array(LED_RUMBLE).buffer);
+				console.log(LED_RUMBLE);
     }
 
 
     ext.rumble_on = function() {
-  //      rumble=1;
         setTimeout(rumble_off, 1000);
     }
 
     ext.rumble_off = function() {
-  //    rumble=0;
     }
 
 
@@ -162,7 +151,7 @@ var GETSTATE=[0x15,0x00];
       //    ['r', '赤外線ポインタの%m.ir_axis 軸の座標','send_ir_axis','x'],
       //    ['b', 'リモコンが画面が向いている','send_ir_find'],
       //      ['r', '加速度の大きさ','send_accel_scale_magnitude','a'],
-    //        [' ', 'LEDを %m.led で点灯 ','trunOnLED','□□□□'],
+            [' ', 'LEDを %m.led で点灯 ','trunOnLED','□□□□'],
       //      [' ', 'モーターを振動させる ','rumble_on'],
             ['h', '%m.button ボタンが押されたとき', 'when_push','a'],
         ],
